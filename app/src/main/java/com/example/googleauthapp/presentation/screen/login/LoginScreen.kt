e44214d5778160d33b5e53421ad03210ab541aa8
Nirvik Basnet <46699486+nirvikBasnet@users.noreply.github.com>
2023-01-06 14:29:38 +1100

    Added StartActivityForResult logic to LoginScreen

--- app/src/main/java/com/example/googleauthapp/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/googleauthapp/presentation/screen/login/LoginScreen.kt
@@ -1,13 +1,16 @@
 package com.example.googleauthapp.presentation.screen.login
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-
+import com.example.googleauthapp.presentation.screen.common.StartActivityForResult
+import com.example.googleauthapp.presentation.screen.common.signIn
 
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -31,4 +34,29 @@ fun LoginScreen(navController: NavController, loginViewModel: LoginViewModel = h
             )
         }
     )
+
+    val activity = LocalContext.current as Activity
+
+    StartActivityForResult(
+        signedInState,
+        onResultReceived = {
+            tokenId ->
+
+        },
+        onDialogDismissed = {
+            loginViewModel.saveSignedInState(false)
+        }
+    ){ activityLauncher ->
+        if(signedInState){
+            signIn(
+                activity =activity,
+                launchActivityResult = {
+                    activityLauncher.launch(it)
+                },
+                accountNotFound = {
+                    loginViewModel.saveSignedInState(false)
+                }
+            )
+        }
+    }
 }
